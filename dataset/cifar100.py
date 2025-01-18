a23be6fdc40ebe4b194bc189ed962732df2fd8cd
Adam Rahmoun <148794815+arahmoun2002@users.noreply.github.com>
2025-01-18 19:53:06 +0100

    final commit

--- dataset/cifar100.py
+++ dataset/cifar100.py
@@ -9,7 +9,7 @@ class MyCIFAR100(CIFAR100):
     """
     Overrides the CIFAR100 dataset to change the getitem function.
     """
-    def __init__(self, root, train=True, transform=None, download=False):          
+    def __init__(self, root, train=True, transform=None, download=True):          
         super(MyCIFAR100, self).__init__(root=root, train=train, transform=transform, download=download)
         self.transform = transform
         
